9b4781da8269cd159f7cf3185c19913af9607042
anthonyygpz@gmail.com <anthonyygpz@gmail.com>
2025-03-14 14:59:38 -0700

    refactor(apiClient.ts): Cambio de la url de la api

--- front-end/src/libs/apiClient.ts
+++ front-end/src/libs/apiClient.ts
@@ -1,16 +1,38 @@
-import axios, { AxiosInstance, AxiosResponse, AxiosError } from "axios";
+import axios, {
+  AxiosInstance,
+  AxiosResponse,
+  AxiosError,
+  InternalAxiosRequestConfig,
+} from "axios";
 
 const api_url = import.meta.env.VITE_APP_API_URL;
 
-const apiQuote: AxiosInstance = axios.create({
-  baseURL: `${api_url}api_v1`, // URL de tu API
+const token = sessionStorage.getItem("token");
+
+const apiClient: AxiosInstance = axios.create({
+  baseURL: `${api_url}api/v1`, // URL de tu API
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${token} `,
   },
 });
 
+// Interceptor de solicitud para actualizar el token antes de cada solicitud
+apiClient.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
 // Interceptor para manejar errores globalmente
-apiQuote.interceptors.response.use(
+apiClient.interceptors.response.use(
   (response: AxiosResponse) => response,
   (error: AxiosError) => {
     console.error("API Error:", error.message);
@@ -18,4 +40,4 @@ apiQuote.interceptors.response.use(
   },
 );
 
-export default apiQuote;
+export default apiClient;
