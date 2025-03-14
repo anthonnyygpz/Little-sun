44b7faa869a66fffd755b816724836316a74518b
anthonyygpz@gmail.com <anthonyygpz@gmail.com>
2025-03-14 14:49:12 -0700

    feat(headers.tsx): Agrego el boton de cerrar sesion

--- front-end/src/features/shared/components/header.tsx
+++ front-end/src/features/shared/components/header.tsx
@@ -1,19 +1,34 @@
 import React from "react";
-import { Table2 } from "lucide-react";
+import { LogOutIcon, Table2 } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 interface HeaderProps {
   site?: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ site }) => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
     <header>
-      <Link to={"/"}>
-        <h2 className="logo">
-          <Table2 /> Little Sun App - {site}
-        </h2>
-      </Link>
+      <div className="container-navbar">
+        <Link to={"/"}>
+          <h2 className="logo">
+            <Table2 /> Little Sun App - {site}
+          </h2>
+        </Link>
+        <button onClick={handleLogout}>
+          <div className="container-buttons">
+            <LogOutIcon />
+            <span>Cerrar Sesion</span>
+          </div>
+        </button>
+      </div>
     </header>
   );
 };
