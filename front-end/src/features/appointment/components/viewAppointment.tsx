bb4daaec1efc698dcfbe99039bcbe01dee9d3ba4
anthonyygpz@gmail.com <anthonyygpz@gmail.com>
2025-03-17 00:28:36 -0700

    refactor(viewAppointment.tsx): eliminado todo lo que tenia que ver con MenuHamburguer

--- front-end/src/features/appointment/components/viewAppointment.tsx
+++ front-end/src/features/appointment/components/viewAppointment.tsx
@@ -1,11 +1,9 @@
 import DefaultLayout from "../../shared/components/defaultLayout.tsx";
-import { MenuHamburguer } from "../../shared/components/menuHamburguer.tsx";
 import TableAppointment from "../../shared/components/tableAppointment/tableAppointment.tsx";
 const ViewAppointment: React.FC = () => {
   return (
     <DefaultLayout site="Citas">
       <TableAppointment />
-      <MenuHamburguer />
     </DefaultLayout>
   );
 };
